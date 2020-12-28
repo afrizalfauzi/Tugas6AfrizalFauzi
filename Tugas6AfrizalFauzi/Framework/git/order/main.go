b43ec43f1b264de156c7f710627af344a7c9ec20
afrizalfauzi <72583908+afrizalfauzi@users.noreply.github.com>
2020-12-28 20:52:44 +0700

    Add files via upload

---
+++ Tugas6AfrizalFauzi/Framework/git/order/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	cm "Tugas6AfrizalFauzi/Framework/git/order/common"
+	"Tugas6AfrizalFauzi/Framework/git/order/middleware"
+	"Tugas6AfrizalFauzi/Framework/git/order/services"
+	"Tugas6AfrizalFauzi/Framework/git/order/transport"
+
+	log "github.com/Sirupsen/logrus"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+func initHandlers() {
+
+	var svc services.PaymentServices
+
+	svc = services.PaymentService{}
+	svc = middleware.BasicMiddleware()(svc)
+
+	root := cm.Config.RootURL
+
+	http.Handle(fmt.Sprintf("%s/orders", root), httptransport.NewServer(
+		transport.OrderEndpoint(svc), transport.DecodeRequest, transport.EncodeResponse,
+	))
+
+	//Handler baru customer
+	http.Handle(fmt.Sprintf("%s/customers", root), httptransport.NewServer(
+		transport.CustomerEndpoint(svc), transport.DecodeCustomerRequest, transport.EncodeResponse,
+	))
+
+	//Handler baru Product
+	http.Handle(fmt.Sprintf("%s/products", root), httptransport.NewServer(
+		transport.ProductEndpoint(svc), transport.DecodeProductRequest, transport.EncodeResponse,
+	))
+
+	//fastpay handler
+	http.Handle(fmt.Sprintf("%s/fastpay", root), httptransport.NewServer(
+		transport.FastEndpoint(svc), transport.DecodeFastPayRequest, transport.EncodeResponse,
+	))
+
+	http.Handle(fmt.Sprintf("%s/call", root), httptransport.NewServer(
+		transport.CallEndpoint(svc), transport.DecodeCallRequest, transport.EncodeResponse,
+	))
+
+	//InquiryPayment handler
+	http.Handle(fmt.Sprintf("%s/payment", root), httptransport.NewServer(
+		transport.InquiryEndpoint(svc), transport.DecodeInquiryPaymentRequest, transport.EncodeResponse,
+	))
+
+	http.Handle(fmt.Sprintf("%s/trips", root), httptransport.NewServer(
+		transport.TripsEndpoint(svc), transport.DecodeTripRequest, transport.EncodeResponse,
+	))
+
+}
+
+var logger *log.Entry
+
+func initLogger() {
+	log.SetFormatter(&log.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05.999",
+	})
+
+	//log.SetReportCaller(true)
+}
+
+func main() {
+
+	configFile := flag.String("conf", "conf-dev.yml", "main configuration file")
+	flag.Parse()
+	initLogger()
+	log.WithField("file", *configFile).Info("Loading configuration file")
+	cm.LoadConfigFromFile(configFile)
+	initHandlers()
+
+	var err error
+	if cm.Config.RootURL != "" || cm.Config.ListenPort != "" {
+		err = http.ListenAndServe(cm.Config.ListenPort, nil)
+	}
+
+	if err != nil {
+		log.WithField("error", err).Error("Unable to start the server")
+		os.Exit(1)
+	}
+
+}
